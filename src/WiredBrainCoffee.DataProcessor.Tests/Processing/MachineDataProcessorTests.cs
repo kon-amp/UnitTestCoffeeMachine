f0058ca35c1d690a54cc010b609552e19042bdce
kostis.ampatzidis <kon.ampatzidis@gmail.com>
2024-10-11 14:44:41 +0300

    feat; add unit test for future implementation of ignoring items that are not newer (TDD-development) logic

--- src/WiredBrainCoffee.DataProcessor.Tests/Processing/MachineDataProcessorTests.cs
+++ src/WiredBrainCoffee.DataProcessor.Tests/Processing/MachineDataProcessorTests.cs
@@ -57,6 +57,33 @@ public class MachineDataProcessorTests : IDisposable {
         }
     }
 
+    [Fact]
+    public void ShouldIgnoreItemsThatAreNotNewer() {
+        // Arrange
+        var items = new[] {
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,8,0,0)),
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,0,0)),  // should be ignore
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,7,10,0)), // should be ignore
+            new MachineDataItem("Cappuccino", new DateTime(2022,10,27,9,0,0)),
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0)),   // should be ignore
+            new MachineDataItem("Espresso", new DateTime(2022,10,27,10,0,0))    // should be ignore
+        };
+
+        // Act
+        _machineDataProcessor.ProcessItems(items);
+
+        // Assert
+        Assert.Equal(2, _coffeeCountStore.SaveItems.Count);
+
+        var item = _coffeeCountStore.SaveItems[0];
+        Assert.Equal("Cappuccino", item.CoffeeType);
+        Assert.Equal(2, item.Count);
+
+        item = _coffeeCountStore.SaveItems[1];
+        Assert.Equal("Espresso", item.CoffeeType);
+        Assert.Equal(1, item.Count);
+    }
+
     public void Dispose() {
         // This runs after every test
     }
