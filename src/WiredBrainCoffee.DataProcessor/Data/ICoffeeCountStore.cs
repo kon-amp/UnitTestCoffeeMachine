23adcb0f0caa554c35a5d30b1e617a6a9280200d
kostis.ampatzidis <kon.ampatzidis@gmail.com>
2024-10-11 13:07:46 +0300

    feat: decouple dependecies to console from MachineDataProcessor so we can test it and to be aligned with single logic programming

---
+++ src/WiredBrainCoffee.DataProcessor/Data/ICoffeeCountStore.cs
@@ -0,0 +1,6 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Data;
+public interface ICoffeeCountStore {
+    void Save(CoffeeCountItem item);
+}
--- src/WiredBrainCoffee.DataProcessor/Processing/MachineDataProcessor.cs
+++ src/WiredBrainCoffee.DataProcessor/Processing/MachineDataProcessor.cs
@@ -1,42 +1,33 @@
-﻿using WiredBrainCoffee.DataProcessor.Model;
+﻿using WiredBrainCoffee.DataProcessor.Data;
+using WiredBrainCoffee.DataProcessor.Model;
 
-namespace WiredBrainCoffee.DataProcessor.Processing
-{
-    public class MachineDataProcessor
-    {
-        private readonly Dictionary<string, int> _countPerCoffeeType = new();
+namespace WiredBrainCoffee.DataProcessor.Processing;
 
-        public void ProcessItems(MachineDataItem[] dataItems)
-        {
-            _countPerCoffeeType.Clear();
+public class MachineDataProcessor(ICoffeeCountStore coffeeCountStore) {
+    private readonly Dictionary<string, int> _countPerCoffeeType = [];
 
-            foreach (var dataItem in dataItems)
-            {
-                ProcessItem(dataItem);
-            }
+    public void ProcessItems(MachineDataItem[] dataItems) {
+        _countPerCoffeeType.Clear();
 
-            SaveCountPerCoffeeType();
+        foreach (var dataItem in dataItems) {
+            ProcessItem(dataItem);
         }
 
-        private void ProcessItem(MachineDataItem dataItem)
-        {
-            if (!_countPerCoffeeType.ContainsKey(dataItem.CoffeeType))
-            {
-                _countPerCoffeeType.Add(dataItem.CoffeeType, 1);
-            }
-            else
-            {
-                _countPerCoffeeType[dataItem.CoffeeType]++;
-            }
-        }
+        SaveCountPerCoffeeType();
+    }
 
-        private void SaveCountPerCoffeeType()
-        {
-            foreach (var entry in _countPerCoffeeType)
-            {
-                var line = $"{entry.Key}:{entry.Value}";
-                Console.WriteLine(line);
-            }
+    private void ProcessItem(MachineDataItem dataItem) {
+        if (!_countPerCoffeeType.ContainsKey(dataItem.CoffeeType)) {
+            _countPerCoffeeType.Add(dataItem.CoffeeType, 1);
+        }
+        else {
+            _countPerCoffeeType[dataItem.CoffeeType]++;
+        }
+    }
+
+    private void SaveCountPerCoffeeType() {
+        foreach (var entry in _countPerCoffeeType) {
+            coffeeCountStore.Save(new CoffeeCountItem(entry.Key, entry.Value));
         }
     }
 }
