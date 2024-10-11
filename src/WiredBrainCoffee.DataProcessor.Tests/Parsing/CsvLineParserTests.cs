583bc4148892b8934402fef324dfdf1c2b2d9f72
kostis.ampatzidis <kon.ampatzidis@gmail.com>
2024-10-11 11:46:19 +0300

    feat: add a first unit about csv line parse method

---
+++ src/WiredBrainCoffee.DataProcessor.Tests/Parsing/CsvLineParserTests.cs
@@ -0,0 +1,16 @@
+﻿namespace WiredBrainCoffee.DataProcessor.Parsing;
+public class CsvLineParserTests {
+
+    [Fact]
+    public void ShouldParseValidLine() {
+        string[] csvLines = ["Cappuccino;10/27/2022 8:06:04 AM"];
+
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        Assert.NotNull(machineDataItems);
+        Assert.Single(machineDataItems);
+        Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
+        Assert.Equal(expected: new DateTime(2022, 10, 27, 8, 6, 4), actual: machineDataItems[0].CreatedAt);
+    }
+}
+
--- src/WiredBrainCoffee.DataProcessor/Model/MachineDataItem.cs
+++ src/WiredBrainCoffee.DataProcessor/Model/MachineDataItem.cs
@@ -1,7 +1,7 @@
 ﻿namespace WiredBrainCoffee.DataProcessor.Model;
 
 /// <summary>
-/// This method performs a specific action. For more details, visit 
+/// For more details on C# records, visit 
 /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/records">this link</see>.
 /// </summary>
 public record MachineDataItem(string CoffeeType, DateTime CreatedAt);
