ff0778eebb584fa31de2ddf6f282e680868c0397
kostis.ampatzidis <kon.ampatzidis@gmail.com>
2024-10-11 13:53:43 +0300

    feat: How to test console output logic

---
+++ src/WiredBrainCoffee.DataProcessor.Tests/Data/ConsoleCoffeeeCountStoreTests.cs
@@ -0,0 +1,20 @@
+﻿using WiredBrainCoffee.DataProcessor.Model;
+
+namespace WiredBrainCoffee.DataProcessor.Data; 
+public class ConsoleCoffeeeCountStoreTests {
+
+    [Fact]
+    public void ShouldWriteOutputToConsole() {
+        // Arrange
+        var item = new CoffeeCountItem("Cappuccino", 5);
+        var stringWriter = new StringWriter();
+        var consoleCoffeeeCountStore = new ConsoleCoffeeCountStore(stringWriter);
+
+        // Act
+        consoleCoffeeeCountStore.Save(item);
+
+        // Assert
+        var result = stringWriter.ToString();
+        Assert.Equal($"{item.CoffeeType}:{item.Count}{Environment.NewLine}", result);
+    }
+}
--- src/WiredBrainCoffee.DataProcessor/Data/ConsoleCoffeeCountStore.cs
+++ src/WiredBrainCoffee.DataProcessor/Data/ConsoleCoffeeCountStore.cs
@@ -3,8 +3,15 @@
 namespace WiredBrainCoffee.DataProcessor.Data;
 
 public class ConsoleCoffeeCountStore : ICoffeeCountStore {
+    private readonly TextWriter _textWriter;
+
+    public ConsoleCoffeeCountStore() : this(Console.Out) { }
+
+    public ConsoleCoffeeCountStore(TextWriter textWriter) {
+        _textWriter = textWriter;
+    }
     public void Save(CoffeeCountItem item) {
         var line = $"{item.CoffeeType}:{item.Count}";
-        Console.WriteLine(line);
-    }   
+        _textWriter.WriteLine(line);
+    }
 }
