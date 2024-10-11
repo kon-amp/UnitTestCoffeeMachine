e165a695b47694d0107ab448ad32418ca4c157b9
kostis.ampatzidis <kon.ampatzidis@gmail.com>
2024-10-11 11:57:29 +0300

    feat: add test to check csv parse to handle empty lines and spaces and as a result we fixed also the related method

--- src/WiredBrainCoffee.DataProcessor.Tests/Parsing/CsvLineParserTests.cs
+++ src/WiredBrainCoffee.DataProcessor.Tests/Parsing/CsvLineParserTests.cs
@@ -15,5 +15,18 @@ public class CsvLineParserTests {
         Assert.Equal("Cappuccino", machineDataItems[0].CoffeeType);
         Assert.Equal(expected: new DateTime(2022, 10, 27, 8, 6, 4), actual: machineDataItems[0].CreatedAt);
     }
+
+    [Fact]
+    public void ShouldSkipEmptyLines() {
+        // Arrange
+        string[] csvLines = ["", " "];
+
+        // Act
+        var machineDataItems = CsvLineParser.Parse(csvLines);
+
+        // Assert
+        Assert.NotNull(machineDataItems);
+        Assert.Empty(machineDataItems);
+    }
 }
 
